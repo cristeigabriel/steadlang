94516ee5c958c6bc9df1285a485e182835f71568
Gabriel <51248378+cristeigabriel@users.noreply.github.com>
2021-01-21 01:01:10 +0200

    prevent leaks

--- main.c
+++ main.c
@@ -7,22 +7,32 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  struct _lexer_instance *lexer_instance =
+      (struct _lexer_instance *)malloc(sizeof(struct _lexer_instance));
+
+  struct _tokenizer_instance *tokenizer_instance =
+      (struct _tokenizer_instance *)malloc(sizeof(struct _tokenizer_instance));
+
+  if (lexer_instance == NULL) {
+    logger_log(error,
+               "failed to initialize lexer instance (%p), returned NULL\n",
+               &lexer_instance);
+    goto destruct;
+  }
+
+  if (tokenizer_instance == NULL) {
+    logger_log(error,
+               "failed to initialize tokenizer instance (%p), returned NULL\n",
+               &tokenizer_instance);
+    goto destruct;
+  }
+
   struct _lexer_settings lexer_settings;
   lexer_settings.file_expected_extension = ".sl";
   lexer_settings.file_extension_mismatch = warning;
   lexer_settings.file_read_safety = true;
   lexer_settings.file_read_sizes_mismatch = error;
   lexer_settings.file_size_too_big_error = true;
-
-  struct _lexer_instance *lexer_instance =
-      (struct _lexer_instance *)malloc(sizeof(struct _lexer_instance));
-
-  if (lexer_instance == NULL) {
-    logger_log(error,
-               "failed to initialize lexer instance (%p), returned NULL\n",
-               &lexer_instance);
-    return 0;
-  }
 
   lexer_instance->lexer_settings = lexer_settings;
 
@@ -31,16 +41,6 @@ int main(int argc, char **argv) {
   tokenizer_settings.scope = "scope";
   tokenizer_settings.scope_delimiter = "::";
 
-  struct _tokenizer_instance *tokenizer_instance =
-      (struct _tokenizer_instance *)malloc(sizeof(struct _tokenizer_instance));
-
-  if (tokenizer_instance == NULL) {
-    logger_log(error,
-               "failed to initialize tokenizer instance (%p), returned NULL\n",
-               &tokenizer_instance);
-    return 0;
-  }
-
   tokenizer_instance->tokenizer_settings = tokenizer_settings;
   tokenizer_instance->lexer = lexer_instance;
 
@@ -48,16 +48,17 @@ int main(int argc, char **argv) {
     printf("%s\n", lexer_instance->file);
   else {
     logger_log(error, "lexer initializer returned false\n");
-    return 0;
+    goto destruct;
   }
 
   if (tokenizer_initialize(tokenizer_instance))
     ;
   else {
     logger_log(error, "tokenizer initializer returned false\n");
-    return 0;
+    goto destruct;
   }
 
+destruct:
   free(lexer_instance->file);
   free(lexer_instance);
   free(tokenizer_instance);
